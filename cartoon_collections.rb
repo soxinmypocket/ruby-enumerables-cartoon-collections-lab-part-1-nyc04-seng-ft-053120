6d5fc5ad5631992dd4d471e54c0c25936080b55a
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-12 16:12:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
 characters_array = ["Hoggle", "Ludo", "Sir Didymus"]
   characters_array.each do |greeting|
-    print "Hello #{greeting}!"
+    p "Hello #{greeting}!"
 end
 
 def list_dwarves(array)
